535be725440e99025d1e2a51662b25552b722e01
javad <alirezaeyan.javad@gmail.com>
2019-08-07 10:25:04 +0430

    configure EmailTableSeeder to add some records to database

--- email/database/seeds/EmailTableSeeder.php
+++ email/database/seeds/EmailTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Email;
 class EmailTableSeeder extends Seeder
 {
     /**
@@ -11,6 +11,24 @@ class EmailTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Let's truncate our existing records to start from scratch.
+        Email::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        $recv = [];
+        for($i=1; $i <= rand(1, 10); $i++){
+            $recv[] =  $faker->freeEmail;
+        }
+        // And now, let's create a few articles in our database:
+        for ($i = 0; $i < 50; $i++) {
+            Email::create([
+                'email_title' => $faker->sentence,
+                'email_body' => $faker->paragraph,
+                'email_receiver' => json_encode($recv),
+                'email_state'=> $faker->numberBetween(0,2),
+                'email_service'=> $faker->numberBetween(0,1)
+            ]);
+        }
     }
 }
