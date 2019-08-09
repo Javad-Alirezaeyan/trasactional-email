5da0e2c1c841de6409984f611fd417d76734e9c2
javad <alirezaeyan.javad@gmail.com>
2019-08-09 21:00:41 +0430

    compose.blade.php

--- email/resources/views/email/compose.blade.php
+++ email/resources/views/email/compose.blade.php
@@ -8,24 +8,6 @@
 
 ?>
 
+<compose-email>
 
-<div class="card-block">
-    <h3 class="card-title">Compose New Message</h3>
-    <div class="form-group">
-        <input class="form-control" placeholder="To:">
-    </div>
-    <div class="form-group">
-        <input class="form-control" placeholder="Subject:">
-    </div>
-    <div class="form-group">
-        <textarea class="textarea_editor form-control" rows="15" placeholder="Enter text ..."></textarea>
-    </div>
-    <h4><i class="ti-link"></i> Attachment</h4>
-    <form action="#" class="dropzone">
-        <div class="fallback">
-            <input name="file" type="file" multiple />
-        </div>
-    </form>
-    <button type="submit" class="btn btn-success m-t-20"><i class="fa fa-envelope-o"></i> Send</button>
-    <button class="btn btn-inverse m-t-20"><i class="fa fa-times"></i> Discard</button>
-</div>
+</compose-email>
