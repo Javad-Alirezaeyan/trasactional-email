ad10384565e54d50313c3c4bd5bcd73a0a03e8a0
javad <alirezaeyan.javad@gmail.com>
2019-08-09 11:36:17 +0430

    distract the email frame of other files, and update the  compose and table files matching with EmailFrame

--- email/resources/views/email/table.blade.php
+++ email/resources/views/email/table.blade.php
@@ -7,75 +7,36 @@
  */
 
 ?>
+<div class="col-xlg-10 col-lg-8 col-md-8">
+    <div class="card-block">
+        <div class="btn-group m-b-10 m-r-10" role="group" aria-label="Button group with nested dropdown">
+            <button type="button" class="btn btn-secondary font-18 text-dark"><i class="mdi mdi-inbox-arrow-down"></i></button>
+            <button type="button" class="btn btn-secondary font-18 text-dark"><i class="mdi mdi-alert-octagon"></i></button>
+            <button type="button" class="btn btn-secondary font-18 text-dark"><i class="mdi mdi-delete"></i></button>
+        </div>
+        <div class="btn-group m-b-10 m-r-10" role="group" aria-label="Button group with nested dropdown">
+            <div class="btn-group" role="group">
+                <button id="btnGroupDrop1" type="button" class="btn btn-secondary text-dark dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-folder font-18 "></i> </button>
+                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1"> <a class="dropdown-item" href="#">Dropdown link</a> <a class="dropdown-item" href="#">Dropdown link</a> </div>
+            </div>
+            <div class="btn-group" role="group">
+                <button id="btnGroupDrop1" type="button" class="btn text-dark btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-label font-18"></i> </button>
+                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1"> <a class="dropdown-item" href="#">Dropdown link</a> <a class="dropdown-item" href="#">Dropdown link</a> </div>
+            </div>
+        </div>
+        <button type="button " class="btn btn-secondary m-r-10 m-b-10 text-dark"><i class="mdi mdi-reload font-18"></i></button>
+        <div class="btn-group" role="group">
+            <button id="btnGroupDrop1" type="button" class="btn m-b-10 text-dark btn-secondary p-10 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> More </button>
+            <div class="dropdown-menu" aria-labelledby="btnGroupDrop1"> <a class="dropdown-item" href="#">Mark as all read</a> <a class="dropdown-item" href="#">Dropdown link</a> </div>
+        </div>
+    </div>
+    <div class="card-block p-t-0">
+        <div class="card b-all shadow-none">
+            <div class="inbox-center table-responsive">
+                <table-email>
 
-@extends("layouyts.master");
-@section('title', "Email List");
-
-@section('content')
-    <!-- Start Page Content -->
-    <!-- ============================================================== -->
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="card">
-                <div class="row">
-                    <div class="col-xlg-2 col-lg-4 col-md-4">
-                        <div class="card-block inbox-panel"><a href="app-compose.html" class="btn btn-danger m-b-20 p-10 btn-block waves-effect waves-light">Compose</a>
-                            <ul class="list-group list-group-full">
-                               <!-- <li class="list-group-item active"> <a href="javascript:void(0)"><i class="mdi mdi-gmail"></i> Inbox </a><span class="badge badge-success ml-auto">6</span></li>-->
-                                <li class="list-group-item ">
-                                    <a href="javascript:void(0)"> <i class="mdi mdi-file-document-box"></i> Sent Mail </a>
-                                </li>
-                                <li class="list-group-item">
-                                    <a href="javascript:void(0)"> <i class="mdi mdi-star"></i> Starred </a>
-                                </li>
-                                <li class="list-group-item">
-                                    <a href="javascript:void(0)"> <i class="mdi mdi-send"></i> Draft </a><span class="badge badge-danger ml-auto">3</span></li>
-
-                                <li class="list-group-item">
-                                    <a href="javascript:void(0)"> <i class="mdi mdi-delete"></i> Trash </a>
-                                </li>
-                            </ul>
-                            <h3 class="card-title m-t-40">Labels</h3>
-                            <div class="list-group b-0 mail-list"> <a href="#" class="list-group-item"><span class="fa fa-circle text-info m-r-10"></span>Work</a> <a href="#" class="list-group-item"><span class="fa fa-circle text-warning m-r-10"></span>Family</a> <a href="#" class="list-group-item"><span class="fa fa-circle text-purple m-r-10"></span>Private</a> <a href="#" class="list-group-item"><span class="fa fa-circle text-danger m-r-10"></span>Friends</a> <a href="#" class="list-group-item"><span class="fa fa-circle text-success m-r-10"></span>Corporate</a> </div>
-                        </div>
-                    </div>
-                    <div class="col-xlg-10 col-lg-8 col-md-8">
-                        <div class="card-block">
-                            <div class="btn-group m-b-10 m-r-10" role="group" aria-label="Button group with nested dropdown">
-                                <button type="button" class="btn btn-secondary font-18 text-dark"><i class="mdi mdi-inbox-arrow-down"></i></button>
-                                <button type="button" class="btn btn-secondary font-18 text-dark"><i class="mdi mdi-alert-octagon"></i></button>
-                                <button type="button" class="btn btn-secondary font-18 text-dark"><i class="mdi mdi-delete"></i></button>
-                            </div>
-                            <div class="btn-group m-b-10 m-r-10" role="group" aria-label="Button group with nested dropdown">
-                                <div class="btn-group" role="group">
-                                    <button id="btnGroupDrop1" type="button" class="btn btn-secondary text-dark dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-folder font-18 "></i> </button>
-                                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1"> <a class="dropdown-item" href="#">Dropdown link</a> <a class="dropdown-item" href="#">Dropdown link</a> </div>
-                                </div>
-                                <div class="btn-group" role="group">
-                                    <button id="btnGroupDrop1" type="button" class="btn text-dark btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-label font-18"></i> </button>
-                                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1"> <a class="dropdown-item" href="#">Dropdown link</a> <a class="dropdown-item" href="#">Dropdown link</a> </div>
-                                </div>
-                            </div>
-                            <button type="button " class="btn btn-secondary m-r-10 m-b-10 text-dark"><i class="mdi mdi-reload font-18"></i></button>
-                            <div class="btn-group" role="group">
-                                <button id="btnGroupDrop1" type="button" class="btn m-b-10 text-dark btn-secondary p-10 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> More </button>
-                                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1"> <a class="dropdown-item" href="#">Mark as all read</a> <a class="dropdown-item" href="#">Dropdown link</a> </div>
-                            </div>
-                        </div>
-                        <div class="card-block p-t-0">
-                            <div class="card b-all shadow-none">
-                                <div class="inbox-center table-responsive">
-                                    <table-email>
-
-                                    </table-email>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                </table-email>
             </div>
         </div>
     </div>
-
-
-@endsection
+</div>
