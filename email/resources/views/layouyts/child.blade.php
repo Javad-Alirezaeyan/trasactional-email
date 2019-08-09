15ec15d6852e998e4cbd87d10413625d8b933594
javad <alirezaeyan.javad@gmail.com>
2019-08-09 10:52:15 +0430

    Add a layout including : master, head, footer, ...

---
+++ email/resources/views/layouyts/child.blade.php
@@ -0,0 +1,14 @@
+@extends("layouyts.master");
+
+@section("title")
+    hello Javad
+    @endsection
+
+@section("sidebar")
+    @parent
+    new sidebar
+    @endsection
+
+@section("content")
+    content
+    @endsection
---
+++ email/resources/views/layouyts/master.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html  lang="en" >
+
+<head>
+ @include("layouyts.partials.head")
+</head>
+
+<body class="fix-header card-no-border logo-center">
+<div class="preloader">
+    <svg class="circular" viewBox="25 25 50 50">
+        <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
+</div>
+<div id="main-wrapper">
+    @include("layouyts.partials.header")
+@include("layouyts.partials.nav")
+    <div id="app">
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Bread crumb and right sidebar toggle -->
+                <!-- ============================================================== -->
+                <div class="row page-titles">
+                    <div class="col-md-5 col-8 align-self-center">
+                        <h3 class="text-themecolor m-b-0 m-t-0">@yield("title")</h3>
+                        <ol class="breadcrumb">
+
+
+                            <li class="breadcrumb-item">
+                                <a href="<?php  echo "a" ?>">
+                                    <?php  echo "test"; ?>
+                                </a>
+                            </li>
+                            <li class="breadcrumb-item active">
+                                <?php
+                                echo "test";
+                                ?>
+                            </li>
+                        </ol>
+                    </div>
+                    <div class="col-md-7 col-4 align-self-center">
+                        <div class="d-flex m-t-10 justify-content-end">
+                            <div class="d-flex m-r-20 m-l-10 hidden-md-down">
+                                <div class="chart-text m-r-10">
+                                    <h6 class="m-b-0"><small>ماه جاری</small></h6>
+                                    <h4 class="m-t-0 text-info"><?php echo "12";?></h4></div>
+                                <div class="spark-chart">
+                                    <div id="monthchart"></div>
+                                </div>
+                            </div>
+                            <div class="d-flex m-r-20 m-l-10 hidden-md-down">
+                                <div class="chart-text m-r-10">
+                                    <h6 class="m-b-0"><small>ماه قبل</small></h6>
+                                    <h4 class="m-t-0 text-primary"><?php echo "12" ;?></h4></div>
+                                <div class="spark-chart">
+                                    <div id="lastmonthchart"></div>
+                                </div>
+                            </div>
+                            <div class="">
+                                <button class="right-side-toggle waves-effect waves-light btn-success btn btn-circle btn-sm pull-right m-l-10"><i class="ti-settings text-white"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- End Bread crumb and right sidebar toggle -->
+                <div id="app">
+                    @yield('content')
+                </div>
+
+                <!-- ============================================================== -->
+                <!-- End Right sidebar -->
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            @include('layouyts.partials.footer')
+
+        </div>
+    </div>
+    <script src="{{ asset('js/app.js') }}" ></script>
+
+@yield('js');
+
+@include('layouyts.partials.footer-scripts')
+
+</div>
+
+</body>
+</html>
