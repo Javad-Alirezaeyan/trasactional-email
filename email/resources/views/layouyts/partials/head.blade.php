6bc78d69db78d7e5a9d3ca06a3109d3f1245e9e1
Javad <alirezaeyan.javad@gmail.com>
2019-08-11 20:46:59 +0430

    add baseurl to head

--- email/resources/views/layouyts/partials/head.blade.php
+++ email/resources/views/layouyts/partials/head.blade.php
@@ -6,7 +6,13 @@ $themePath = "theme/";
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}" />
-<script > window.Laravel='{{ csrf_token() }}'</script>
+<script >
+    window.Laravel =
+    <?php echo json_encode([
+        'csrfToken' => csrf_token(),
+        'baseUrl' => URL::to('/')
+    ]); ?>
+</script>
 <!-- Favicon icon -->
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset("theme/assets/images/favicon.png") }}">
 
