fc279348b16b9a8c1cf06a5572fff85767dc44f4
Javad <alirezaeyan.javad@gmail.com>
2019-08-11 23:42:26 +0430

    add some constants to constants.php

--- email/config/constants.php
+++ email/config/constants.php
@@ -7,20 +7,18 @@
  */
 
 
-
 define("EmailQueued", 0);
 define("EmailSent", 1);
 define("EmailDelivered", 2);
 define("EmailNotDelivered", 3);
 define("EmailBounced", 4);
-
 return [
   'EmailState'=>[
       EmailQueued => ['Title'=>'Queued'],
       EmailSent => ['Title'=>'Sent'],
-      EmailDelivered => ['Title'=>'Queued'],
-      EmailNotDelivered => ['Title'=>'Queued'],
-      EmailBounced => ['Title'=>'Queued'],
+      EmailDelivered => ['Title'=>'Delivered'],
+      EmailNotDelivered => ['Title'=>'NotDelivered'],
+      EmailBounced => ['Title'=>'Bounced'],
   ]
 
 ];
