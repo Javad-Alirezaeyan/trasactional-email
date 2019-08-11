dc267aa5ecdd44bbc76fdb24cf4be3d42e144912
Javad <alirezaeyan.javad@gmail.com>
2019-08-11 23:21:54 +0430

    add a file for saving constants

---
+++ email/config/constant.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: javad
+ * Date: 8/11/19
+ * Time: 10:58 PM
+ */
+
+
+
+define("EmailQueued", 0);
+define("EmailSent", 1);
+define("EmailDelivered", 2);
+define("EmailNotDelivered", 3);
+define("EmailBounced", 4);
+
+return [
+  'EmailState'=>[
+      EmailQueued => ['Title'=>'Queued'],
+      EmailSent => ['Title'=>'Sent'],
+      EmailDelivered => ['Title'=>'Queued'],
+      EmailNotDelivered => ['Title'=>'Queued'],
+      EmailBounced => ['Title'=>'Queued'],
+  ]
+
+];
