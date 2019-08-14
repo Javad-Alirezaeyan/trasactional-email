d462fb9fdb2d3121b5b1b9eec546d4be80db37c8
Javad <alirezaeyan.javad@gmail.com>
2019-08-14 15:12:23 +0430

    add more filed to create fake records

--- database/factories/EmailFactory.php
+++ database/factories/EmailFactory.php
@@ -22,10 +22,17 @@ $factory->define(App\Email::class, function (Faker $faker) {
     }
     $to = implode(',', $to);
 
+    $text = $faker->text;
+    $objParse  = new \App\Library\Parsemarkdown();
+    $textparse =  $objParse->text($text);
+
     return [
         'email_subject' => $faker->sentence,
-        'email_contentValue' => $faker->text,
+        'email_contentValue' => $text,
+        'email_orginalContent' => $textparse,
+        'email_state' => EmailQueued,
         'email_to' => $to,
-        'email_from'=> $faker->freeEmail
+        'email_from'=> $faker->freeEmail,
+        'created_at'=> time()
     ];
 });
