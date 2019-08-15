9ddc28a00bdf7a4aa4c721e06396389bec7b1a95
Javad <alirezaeyan.javad@gmail.com>
2019-08-15 10:46:53 +0430

    Remove duplicated directory

--- email/app/MyConstants/constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: javad
- * Date: 8/11/19
- * Time: 10:58 PM
- */
-
-
--- email/app/helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: javad
- * Date: 8/11/19
- * Time: 11:20 PM
- */
-
